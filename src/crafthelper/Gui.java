736f12f47982261fcd66aaa3b7848530e774b6b0
Pyrofab <redstoneinfire@gmail.com>
2017-07-28 12:27:46 +0200

    Update Gui.java

--- src/crafthelper/Gui.java
+++ src/crafthelper/Gui.java
@@ -19,30 +19,31 @@ public class Gui extends JFrame {
 		}
 		Controller control = new Controller(this, data);
 		JPanel main = new JPanel();
-		JPanel craftingGrid = makeCraftingGrid(control);
-		main.add(craftingGrid);
-		JButton result = new JButton("result");
-		result.addActionListener(control::onResultAction);
-		main.add(result);
-		JButton craft = new JButton("make recipe");
-		craft.addActionListener(control::onCompileAction);
-		main.add(craft);
+		{
+			JPanel craftingGrid = new JPanel();
+			craftingGrid.setLayout(new GridLayout(3,3));
+			for(int i = 0; i < 9; i++) {
+				JButton ingredient_button = new JButton();
+				ingredient_button.setActionCommand(String.valueOf(i));
+				ingredient_button.addActionListener(control::ingredientActionEvent);
+				craftingGrid.add(ingredient_button);
+			}
+			main.add(craftingGrid);
+		}
+		{
+			JButton result = new JButton("result");
+			result.addActionListener(control::onResultAction);
+			main.add(result);
+		}
+		{
+			JButton craft = new JButton("make recipe");
+			craft.addActionListener(control::onCompileAction);
+			main.add(craft);
+		}
 		this.setTitle("Crafting-o-tron");
 		this.setContentPane(main);
 		this.pack();
 		this.setVisible(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-	}
-	
-	private JPanel makeCraftingGrid(Controller control) {
-		JPanel craftingGrid = new JPanel();
-		craftingGrid.setLayout(new GridLayout(3,3));
-		for(int i = 0; i < 9; i++) {
-			JButton ingredient_button = new JButton();
-			ingredient_button.setActionCommand(String.valueOf(i));
-			ingredient_button.addActionListener(control::ingredientActionEvent);
-			craftingGrid.add(ingredient_button);
-		}
-		return craftingGrid;
 	}
 }
